6fa9e2635d6ec0dff59d50fcd4a491c2826f765e
morgan <samohodsky2003@gmail.com>
2024-01-14 19:27:05 +0200

    version_2

--- HomeWork_2.py
+++ HomeWork_2.py
@@ -1,14 +1,11 @@
 # Samohodskyi Homework
 nums = int(input())
 num_1 = nums // 1000
-num_2 = nums // 100
-num_3 = nums // 10
-num_4 = nums // 1
-digit_2 = num_2 % 10
-digit_3 = num_3 % 10
-digit_4 = num_4 % 10
+num_2 = (nums // 100) % 10
+num_3 = (nums // 10) % 10
+num_4 = (nums // 1) % 10
 print(num_1)
-print(digit_2)
-print(digit_3)
-print(digit_4)
+print(num_2)
+print(num_3)
+print(num_4)
 
