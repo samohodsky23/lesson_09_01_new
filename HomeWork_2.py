c4a517bf0ef3fc08f9cfd7928c0c4379544cec58
morgan <samohodsky2003@gmail.com>
2024-01-14 19:15:14 +0200

    version_1

---
+++ HomeWork_2.py
@@ -0,0 +1,14 @@
+# Samohodskyi Homework
+nums = int(input())
+num_1 = nums // 1000
+num_2 = nums // 100
+num_3 = nums // 10
+num_4 = nums // 1
+digit_2 = num_2 % 10
+digit_3 = num_3 % 10
+digit_4 = num_4 % 10
+print(num_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
+
---
+++ lesson_2.py
@@ -0,0 +1,51 @@
+# Data types
+
+# Numeric
+
+# int - Integer 1
+# float -  1.3         НЕ точний тип даних - округляє.
+# decimal -  0.01         Більш точний тип даних
+# complex -  ( 1 + 3 )
+
+# bool - logic operator(True/False)
+
+# str - String
+# tuple()     незмінний, як всі вище
+
+# list()      змінний тип даних
+# dict()      словник     змінний тип
+# set()       множина     змінний тип
+
+# range
+
+# None
+
+# box_1_number = 'apples'  # snake case (підкреслювання?)
+# Не можна називати змінні назвою існуючих зміних (print, string, bool etc.)
+# Пиши змінні мінімум 2-ма словами(бажано)
+# Запамʼятати, що таке PEP8 ( правила написання коду (негласні правила), питають на співбесідах )
+
+
+
+# number_1 = 1
+# number_2 = 3.2
+# result_add = number_1 + number_2
+# result_sub = number_1 - number_2
+# result_mul = number_1 * number_2
+# result_div = number_1 / number_2
+
+# result_div_1 = number_1 // number_2    # Цілочисленне ділення
+# result_div_2 = number_1 % number_2     # Залишок від ділення
+
+# result_power = number_1 ** number_2     # ступінь
+# result_root = number_1 ** 0.5      # корінь
+
+# print(result_add, type(result_add))
+# print(result_sub, type(result_sub))
+# print(result_mul, type(result_mul))
+# print(result_div)
+# print(result_div_1)
+
+# result_div_3 = divmod(number_1, number_2)
+
+
