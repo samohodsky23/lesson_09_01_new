c68d3a65d2603f9807b0f2b03ad07c2f747a91b6
morgan <samohodsky2003@gmail.com>
2024-01-17 21:10:30 +0200

    № 1 - version 2.0 | Done № 2 - version 1.0 | Work in Progress

--- HomeWork_3.py
+++ HomeWork_3.py
@@ -28,9 +28,16 @@ list_2 = [1]
 list_3 = []
 list_4 = [12, 3, 4, 10, 8]
 
-list_1.insert(0, list_1.pop(-1))
+if len(list_1) > 1:
+    list_1.insert(0, list_1.pop())
+if len(list_2) > 1:
+    list_2.insert(0, list_2.pop())
+if len(list_3) > 1:
+    list_3.insert(0, list_3.pop())
+if len(list_4) > 1:
+    list_4.insert(0, list_4.pop())
+
 print(list_1)
 print(list_2)
 print(list_3)
-list_4.insert(0, list_4.pop(-1))
 print(list_4)
