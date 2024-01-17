2405fa00cd6467157c02e7c94eb5ef68e90ad6fe
morgan <samohodsky2003@gmail.com>
2024-01-17 21:21:49 +0200

    № 1 - version 2.0 | Done № 2 - version 2.0 | Done

--- HomeWork_3.py
+++ HomeWork_3.py
@@ -21,23 +21,14 @@
 
 
 #############################################################
-# Задача 2. Перемістити елемент у списку | Work in progress
+# Задача 2. Перемістити елемент у списку | Done
 
-list_1 = [12, 3, 4, 10]
-list_2 = [1]
-list_3 = []
-list_4 = [12, 3, 4, 10, 8]
+my_list = [12, 3, 4, 10]        # Змінився
+# my_list = [1]                 # Не змінився
+# my_list = []                  # Не змінився
+# my_list = [12, 3, 4, 10, 8]   # Змінився
 
-if len(list_1) > 1:
-    list_1.insert(0, list_1.pop())
-if len(list_2) > 1:
-    list_2.insert(0, list_2.pop())
-if len(list_3) > 1:
-    list_3.insert(0, list_3.pop())
-if len(list_4) > 1:
-    list_4.insert(0, list_4.pop())
+if len(my_list) > 1:
+    my_list.insert(0, my_list.pop())
 
-print(list_1)
-print(list_2)
-print(list_3)
-print(list_4)
+print(my_list)
