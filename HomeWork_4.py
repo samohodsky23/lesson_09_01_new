7a16773fb7c16ef86c928ea0a36ea4403fb37d2d
morgan <samohodsky2003@gmail.com>
2024-01-22 14:59:59 +0200

    №1. version 1.0 | Done №2. Work in progress

---
+++ HomeWork_4.py
@@ -0,0 +1,13 @@
+# Samohodskyi Homework
+# Задача 1 | Написати програму, яка переміщає всі нулі в кінець списку
+
+# my_list = [0, 1, 0, 12, 3]
+# my_list = [0]
+# my_list = [1, 0, 13, 0, 0, 0, 5]
+my_list = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+
+print(my_list)
+my_list.sort(key=bool, reverse=True)
+print(my_list)
+
+# Задача 2 | Задача з індексами
