a995b9c644700adb155adfde22fa209b85191ba1
morgan <samohodsky2003@gmail.com>
2024-01-29 13:31:41 +0200

    Added lesson_6.

--- homework/homework_5_2.py
+++ homework/homework_5_2.py
@@ -1,40 +1,46 @@
 # Modified calculator
 
-number_1 = float(input())
 
-action = input()
-correct_actions = ['+', '-', '/', '*']
-while action not in correct_actions:
-    print('Такої операції немає в списку! Спробуй ще.')
-    action = input()
-
-number_2 = float(input())
-
-result = float
-
-
-program_is_running = True
+# correct_actions = ['+', '-', '/', '*']
+# while action not in correct_actions:
+#     print('Такої операції немає в списку! Спробуй ще.')
+#     action = input()
 
 
 
-if action == '+':
-    result = number_1 + number_2
+# program_is_running = True
+while True:
+    number_1 = float(input('Enter the first number: '))
 
-elif action == '-':
-    result = number_1 - number_2
-elif action == '*':
-    result = number_1 * number_2
-elif action == '/' and number_2 == 0:
-    result = str('Ділення на нуль неможливе!')
-else:
-    result = number_1 / number_2
+    action = input('Enter the operator: ')
 
+    number_2 = float(input('Enter the second number: '))
+
+    result = float
+
+    if action == '+':
+        result = number_1 + number_2
+        print(result)
+    elif action == '-':
+        result = number_1 - number_2
+        print(result)
+    elif action == '*':
+        result = number_1 * number_2
+        print(result)
+    elif action == '/' and number_2 == 0:
+        result = str('Ділення на нуль неможливе!')
+        print(result)
+    else:
+        result = number_1 / number_2
+        print(result)
+    if input('Do you want to continue? Y/N?') != 'Y':
+        break
 print(result)
 
-while program_is_running:
-
-    answer = input('Бажаєте продовжити? \n'
-                   'Введіть "y", якщо так і будь-який інший, якщо ні: ')
-    if answer != 'y':
-        program_is_running = False
-    else:
+# while program_is_running:
+#
+#     answer = input('Бажаєте продовжити? \n'
+#                    'Введіть "y", якщо так і будь-який інший, якщо ні: ')
+#     if answer != 'y':
+#         program_is_running = False
+#     else:
